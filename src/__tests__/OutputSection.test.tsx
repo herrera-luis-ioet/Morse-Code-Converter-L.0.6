7497a82f48b98a879fee955f99d09c2839ea729e
root <root@ip-172-31-38-202.ec2.internal>
2025-02-17 21:56:34 +0000

    fix(tests): update test files to resolve syntax errors and deprecated usage

--- src/__tests__/OutputSection.test.tsx
+++ src/__tests__/OutputSection.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { act } from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import OutputSection from '../components/OutputSection';
